3665c64cf8546dfacdde74a6ab4ea5ee0ea1d331
Rakotoniaina Rajo Mampionona <rajomampiononarakotoniaina@gmail.com>
2025-05-02 00:05:32 +0300

    Escape apostrophe

--- src/projects/page.tsx
+++ src/projects/page.tsx
@@ -1,10 +1,10 @@
-import ProjectsGrid from '@/components/ProjectsGrid'
+// import ProjectsGrid from '@/components/ProjectsGrid'
 
 export default function ProjectsPage() {
   return (
     <div className="container mx-auto py-12">
       <h1 className="text-4xl font-bold mb-8">Mes Projets</h1>
-      <ProjectsGrid />
+      {/* <ProjectsGrid /> */}
     </div>
-  )
+  );
 }
