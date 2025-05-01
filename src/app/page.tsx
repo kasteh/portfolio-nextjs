dcb40edfc637b478f30fd591a84186668e01fcc8
Rakotoniaina Rajo Mampionona <rajomampiononarakotoniaina@gmail.com>
2025-05-02 00:11:43 +0300

    add vercel speed insight

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,21 @@
-'use client'; // Nécessaire pour utiliser Framer Motion et les hooks React
+"use client"; // Nécessaire pour utiliser Framer Motion et les hooks React
 
-import React from 'react';
-import Hero from '@/components/HeroSection';
-import About from '@/components/About';
-import Skills from '@/components/Skiils';
-import Projects from '@/components/Projects';
-import Contact from '@/components/Contact';
-import Footer from '@/components/Footer';
-
-
-
+import React from "react";
+import Hero from "@/components/HeroSection";
+import About from "@/components/About";
+import Skills from "@/components/Skiils";
+import Projects from "@/components/Projects";
+import Contact from "@/components/Contact";
+import Footer from "@/components/Footer";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 // ---- Page Principale ----
 
 export default function Home() {
   return (
-    <div className="bg-white dark:bg-gray-900"> {/* Conteneur principal pour gérer le thème dark/light si besoin */}
+    <div className="bg-white dark:bg-gray-900">
+      {" "}
+      {/* Conteneur principal pour gérer le thème dark/light si besoin */}
       {/* Un composant Header/Navbar irait bien ici si vous en avez un séparé */}
       <Hero />
       <About />
